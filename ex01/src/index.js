cd437c9cfc29c980ea8922e8f1822dd94c3efc23
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-17 23:59:46 +0100

    Create index.js

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+    var myPet = {
+        species: "Pit Bull",
+        name: "Rex",
+        legs: 4,
+        friends: ["Timmy", "Flufy"]
+ }
+ function (myObj){
+   return myObj;
+ }
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
