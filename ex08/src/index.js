ce08774dbb8780349a0fac1b81cd19fcc3409bdc
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-18 02:26:39 +0100

    Create index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,19 @@
+function getUsername(email){
+  var username = ˙˙;
+  
+  //Only chane code below this line
+  var username = {
+    getUsername("user1@mail.com") =  "user1",
+    getUsername("user2@mail.com") =  "user2",
+    getUsername("user3@mail.com") =  "user3",
+    getUsername("user4@mail.com") =  "user4",
+    getUsername("user5@mail.com") =  "user5",
+    getUsername("user6@mail.com") =  "user6",
+    getUsername("") =  "undefined",
+  }
+   // Only change code above this line
+  
+  return username;
+}
+getUsername("user2@mail.com");
+module.exports = getUsername;
