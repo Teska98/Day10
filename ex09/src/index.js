87bfbdbe2cbb82038be44aa000c70972373be883
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-18 10:48:51 +0100

    Update index.js

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,19 +1,20 @@
 function myFunction(myObj, checkProp){
   // Only change code below this line
-  var myObj {
+  var myObj = {
     "title": "Titanic",
     "song": "My Hearth Will Go On",
     "genre": "drama"          
   };
-  return (checkProp === title) ? "Titanic"
-   :(checkProp === song) ? "My Heart Will Go On"
-   :(checkProp === genre) ? "drama"
+  var checkProp = myObj;
+  return (checkProp === "title") ? "Titanic"
+   :(checkProp === "song") ? "My Heart Will Go On"
+   :(checkProp === "genre") ? "drama"
    : "Not Found";
   
-console.log(myFunction({title: "Titanic", song: "My Hearth Will Go On", genre: "drama" }, "title"));  
-console.log(myFunction({title: "Titanic", song: "My Hearth Will Go On", genre: "drama" }, "song"));  
-console.log(myFunction({title: "Titanic", song: "My Hearth Will Go On", genre: "drama" }, "genre"));  
-console.log(myFunction({title: "Titanic", song: "My Hearth Will Go On", genre: "drama" }, "actor"));  
+myFunction({title: "Titanic", song: "My Hearth Will Go On", genre: "drama" }, "title"));
+myFunction({title: "Titanic", song: "My Hearth Will Go On", genre: "drama" }, "song"));  
+myFunction({title: "Titanic", song: "My Hearth Will Go On", genre: "drama" }, "genre"));  
+myFunction({title: "Titanic", song: "My Hearth Will Go On", genre: "drama" }, "actor"));  
  // Only chane code above this line
 }
 module.exports = myFunction;
